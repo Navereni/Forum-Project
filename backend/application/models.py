c81a122b372d06f8fa276242d6105047435be9e4
Ubuntu <krzysztof@dev-jenkins.rtudhuqav13e5lf1sxn3p0wzyb.fx.internal.cloudapp.net>
2021-12-01 14:55:01 +0000

    trying to add post

--- backend/application/models.py
+++ backend/application/models.py
@@ -1,6 +1,18 @@
 from application import db
 
-class Tasks(db.Model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(30), nullable=False)
-    completed = db.Column(db.Boolean, nullable=False, default=False)
+    text = db.Column(db.String(160), nullable=False)
+    author = db.Column(db.String(15), nullable=False)
+    date_posted = db.Column(db.DateTime), nullable=False)
+    category = db.Column(db.String(10), nullable=False)
+    department = db.Column(db.String(15), nullable=False)
+
+    comments = db.relationship('Comments', backref='posts')
+
+class Comment(db.Model):
+    id = id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(160), nullable=False)
+    author = db.Column(db.String(15), nullable=False)
+    date_posted = db.Column(db.DateTime), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
--- backend/application/routes.py
+++ backend/application/routes.py
@@ -2,7 +2,7 @@ from application import app, db
 from application.models import Tasks
 from flask import render_template, request, redirect, url_for, Response, jsonify
 
-@app.route('/create/task', methods=['POST'])
+@app.route('/create/post', methods=['POST'])
 def create_task():
     package = request.json
     new_task = Tasks(description=package["description"])
@@ -10,57 +10,55 @@ def create_task():
     db.session.commit()
     return Response(f"Added task with description {new_task.description}", mimetype='text/plain')
 
-@app.route('/read/allTasks', methods=['GET'])
-def read_tasks():
-    all_tasks = Tasks.query.all()
-    tasks_dict = {"tasks": []}
-    for task in all_tasks:
-        tasks_dict["tasks"].append(
+@app.route('/create/comment/<int:post_id>', methods=['POST'])
+def create_comment():
+    json = request.json
+    new_comment = Comments(
+        author = json['author'],
+        date_posted = json['date_posted'],
+        post_id = post_id
+    )
+    db.session.add(new.comments)
+    db.session.commit()
+    return f"Added: {new_comment.text} to the post."
+
+@app.route('/read/allPosts', methods=['GET'])
+def read_all_posts():
+    all_posts = Posts.query.all()
+   json = {"posts": []}
+   for post in all_posts:
+       comments = []
+       for comment in posts.comments:
+           comments.append(
+               {
+                   "id": comments.id,
+                   "author": comments.author,
+                   "date_posted": comments.date_posted,
+                   "post_id": comments.posts_id,
+               }
+           )
+        json["posts"].append(
             {
-                "id": task.id,
-                "description": task.description,
-                "completed": task.completed
+                "id": posts.id,
+                
             }
         )
-    return jsonify(tasks_dict)
-
-@app.route('/read/task/<int:id>', methods=['GET'])
-def read_task(id):
-    task = Tasks.query.get(id)
-    tasks_dict = {
-                    "id": task.id,
-                    "description": task.description,
-                    "completed": task.completed
-                }
-    return jsonify(tasks_dict)
 
 
-@app.route('/update/task/<int:id>', methods=['PUT'])
-def update_task(id):
-    package = request.json
-    task = Tasks.query.get(id)
 
-    task.description = package['description']
-    db.session.commit()
-    return Response(f"Updated task (ID: {id}) with description {task.description}", mimetype='text/plain')
+# @app.route('/update/post/<int:id>', methods=['PUT'])
+# def update_task(id):
+#     package = request.json
+#     task = Tasks.query.get(id)
 
-@app.route('/delete/task/<int:id>', methods=['DELETE'])
-def delete_task(id):
-    task = Tasks.query.get(id)
-    db.session.delete(task)
-    db.session.commit()
-    return Response(f"Task (ID: {id}) has been DELETED!", mimetype='text/plain')
+#     post.description = package['description']
+#     db.session.commit()
+#     return Response(f"Updated task (ID: {id}) with description {task.description}", mimetype='text/plain')
+
+# @app.route('/delete/post/<int:id>', methods=['DELETE'])
+# def delete_task(id):
+#     post = Posts.query.get(id)
+#     db.session.delete(task)
+#     db.session.commit()
+#     return Response(f"Task (ID: {id}) has been DELETED!", mimetype='text/plain')
     
-@app.route('/complete/task/<int:id>', methods=['PUT'])
-def complete_task(id):
-    task = Tasks.query.get(id)
-    task.completed = True
-    db.session.commit()
-    return Response(f"Task with (ID: {id}) set to completed = True", mimetype='text/plain')
-
-@app.route('/incomplete/task/<int:id>', methods=['PUT'])
-def incomplete_task(id):
-    task = Tasks.query.get(id)
-    task.completed = False
-    db.session.commit()
-    return Response(f"Task with (ID: {id}) set to completed = False", mimetype='text/plain')
